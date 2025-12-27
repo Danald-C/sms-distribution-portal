cb05dc0cfaa192d354d806016d670a9b4c6a1e09
danald <kwadjodanq@gmail.com>
2025-12-27 22:44:54 +0000

    A slight change on the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,10 @@ import './styles.css'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import AuthProvider from './components/contexts/auth.jsx';
-import LoginPage from './components/loginPage/file.jsx';
-import ProfilePage from './components/profilePage/file.jsx';
-import SignupPage from './components/signupPage/file.jsx';
+// import AuthProvider from './components/contexts/auth.jsx';
+// import LoginPage from './components/loginPage/file.jsx';
+// import ProfilePage from './components/profilePage/file.jsx';
+// import SignupPage from './components/signupPage/file.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
